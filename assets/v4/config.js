780d422b6de2e93db8063220bec4d851940cd125
deputat36 <a.v.kovtun@borisoglebsk.etagi.com>
2026-06-15 16:47:06 +0300

    Increase CRM v4 network tolerance

---
+++ assets/v4/config.js
@@ -0,0 +1,12 @@
+export const V4_CONFIG = Object.freeze({
+  supabaseUrl: 'https://ofewxuqfjhamgerwzull.supabase.co',
+  supabasePublishableKey: 'sb_publishable_ZiX8_Mnf0dY6S__tKO2A4A_uD94G2cs',
+  authStorageKey: 'leader_crm_v4_session',
+  timeouts: Object.freeze({
+    sessionMs: 15000,
+    loginMs: 20000,
+    logoutMs: 8000,
+    profileMs: 12000,
+    requestMs: 20000
+  })
+});
